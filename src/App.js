89dd628428dc00162fe8cf03f53e6010d680b28b
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-07-22 12:08:10 -0400

    Add files and dependencies

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import {connect} from "react-redux"
+
+function App(props) {    
+    return (
+        <div>
+            <h1>COUNT GOES HERE</h1>
+            <button>-</button>
+            <button>+</button>
+        </div>
+    )
+}
+
+export default App
